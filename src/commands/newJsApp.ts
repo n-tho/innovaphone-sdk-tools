d44601209ee5c1c29aaa3fed165fc54ff2e0aeac
Nils Thomsen <nils@nilstho.de>
2026-06-18 23:30:49 +0200

    feat: added check to newJsAppCommand

--- src/commands/newJsApp.ts
+++ src/commands/newJsApp.ts
@@ -1,5 +1,6 @@
 import * as vscode from "vscode";
 import { ProjectService } from "../services/projectService";
+import { SettingsService } from "../services/settingsService";
 
 export function registerNewJsAppCommand(
   context: vscode.ExtensionContext,
@@ -7,13 +8,39 @@ export function registerNewJsAppCommand(
   const command = vscode.commands.registerCommand(
     "innovaphone.newJsApp",
     async () => {
+      const sdkPath = SettingsService.getSdkPath();
+      const companyName = SettingsService.getCompanyName();
+
+      if (!sdkPath) {
+        const action = await vscode.window.showErrorMessage(
+          vscode.l10n.t("Please configure the SDK path first."),
+          vscode.l10n.t("configure"),
+        );
+        if (action) {
+          await vscode.commands.executeCommand("innovaphone.configureSdkPath");
+        }
+        return;
+      }
+
+      if (!companyName) {
+        const action = await vscode.window.showErrorMessage(
+          vscode.l10n.t("Please configure the company name first."),
+          vscode.l10n.t("Configure"),
+        );
+
+        if (action) {
+          await vscode.commands.executeCommand(
+            "innovaphone.configureCompanyName",
+          );
+        }
+        return;
+      }
+
       const appName = await vscode.window.showInputBox({
         prompt: vscode.l10n.t("Name of the innovaphone JS app"),
       });
 
-      if (!appName) {
-        return;
-      }
+      if (!appName) return;
 
       const folder = await vscode.window.showOpenDialog({
         canSelectFiles: false,
@@ -22,9 +49,7 @@ export function registerNewJsAppCommand(
         title: vscode.l10n.t("Select project location"),
       });
 
-      if (!folder?.length) {
-        return;
-      }
+      if (!folder?.length) return;
 
       const projectPath = await ProjectService.createProject(
         folder[0].fsPath,
